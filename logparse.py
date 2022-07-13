4c2388e88f31a8201a99effc9d6dc2d5f6adceb3
darkbahhh <darkbahhh@gmail.com>
2022-07-13 19:04:08 +0200

    add to logparse a function to get action and timestamp from log file lines

--- logparse.py
+++ logparse.py
@@ -5,3 +5,12 @@ def compute_time_diff_seconds(start, end):
     start_time = datetime.datetime.strptime(start, format)
     end_time = datetime.datetime.strptime(end, format)
     return (end_time - start_time).total_seconds()
+
+def get_next_event(filename):
+    with open(filename, "r") as datafile:
+        for line in datafile:
+            if "dut : Device State :" in line:
+                line = line.strip()
+                action = line.split()[-1]
+                timestamp = line[:19]
+                yield (action, timestamp)
