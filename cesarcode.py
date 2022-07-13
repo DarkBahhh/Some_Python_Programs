7c5260966199b89b73d331aa1c11062bce7cae35
darkbahhh <darkbahhh@gmail.com>
2022-07-13 18:31:02 +0200

    Cesar code with ord, chr and while loop

--- cesarcode.py
+++ cesarcode.py
@@ -1,8 +1,23 @@
 import string
 
-# Cesar code with "tanslate" from standard library.
+# Cesar code with "tanslate" methode from standard library.
 def cesar(plain_text, shift_num=1):
     letters = string.ascii_lowercase
     mask = letters[shift_num :] + letters[: shift_num]
     transtab = str.maketrans(letters, mask)
     return plain_text.translate(transtab)
+
+# Cesar code without "translate" methode but with "ord" and "chr" and "while" loop to fix the letter in the lowercase number range of the ASCII code.
+def shift_n(letter, position):
+    if letter not in string.ascii_lowercase:
+        return letter
+    new_letter = ord(letter) + position
+    while new_letter > ord("z"):
+        new_letter -= 26
+    while new_letter < ord("a"):
+        new_letter += 26
+    return chr(new_letter)
+
+def cesar2(message, position):
+    enc_list = [shift_n(letter, position) for letter in message]
+    return "".join(enc_list)
